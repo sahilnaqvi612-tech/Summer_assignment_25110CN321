f2c3fd806eb097fa20551343358a514a232c4a05
sahilnaqvi612-tech <sahilnaqvi612@gmail.com>
2026-06-03 13:15:39 +0530

    day2 q4

--- d2q4.cpp
+++ d2q4.cpp
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+int pallindrome(int);
 int main(){
     int n;
-    cout<<"enter the number: ";
+    cout<<"enter the  number:";
     cin>>n;
-    int ld;
-      int arm=0;
-      int orgnum=n;
-      int temp=n;
-      int cnt=0;
-    while(temp>0){
-        cnt+=1;
-        temp=temp/10;
-    }
-    temp=n;
-
-    while(temp>0){
-         ld=temp%10;
-         arm +=pow(ld,cnt);
-         temp=temp/10;
-    }
-    if(orgnum==arm){
-    cout<<"armstrong";
-    }
-    else{
-        cout<<"not an armstrong number";
-    }
+    pallindrome(n);
+    return 0;
 }
+
+ int pallindrome(int n){
+    int revnum=0;
+    int ld;
+    int orgnum=n;
+    while(n>0){
+    ld=n%10;
+  
+    revnum=revnum*10+ld;
+      n=n/10; 
+}
+
+    if(revnum==orgnum){
+        cout<<"pallindrome";
+    }
+    else 
+   { cout<<"not pallindrome";
+ }
+ return 1;
+ }
